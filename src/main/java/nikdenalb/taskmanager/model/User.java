f21173349c77648132d6ce14b32c35d660eadb53
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-11-27 10:27:19 +0300

    model-DB User

---
+++ src/main/java/nikdenalb/taskmanager/model/User.java
@@ -0,0 +1,35 @@
+package nikdenalb.taskmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
---
+++ src/main/resources/liquibase/scripts/user.sql
@@ -0,0 +1,11 @@
+-- liquibase formatted sql
+
+-- changeset dnikulin:1
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id       BIGINT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password varchar(128) NOT NULL,
+    role     VARCHAR(5)
+);
